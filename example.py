cfd058b95d5916d9607f03bd536f1da3e33cc6c4
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-09-19 20:18:09 -0300

    propagação, disparo, argumentos e alias de exceções

--- example.py
+++ example.py
@@ -1,16 +1,23 @@
 def dividir(dividendo, divisor):
-    return dividendo // divisor
+    if not (isinstance(dividendo, int) and isinstance(divisor, int)):
+        raise ValueError("dividir() deve appenas receber argumentos inteiros.")
+
+    try:
+        return dividendo / divisor
+    except:
+        print(f'Não foi possível dividir {dividendo} por {divisor}')
+        raise
 
 
 def testa_divisao(divisor):
+    resultado = dividir(10, divisor)
+    print(f"O resultado da divisão de 10 por {divisor} é {resultado}")
+
+
+if __name__ == '__main__':
     try:
-        resultado = dividir(10, divisor)
-        print(f"O resultado da divisão de 10 por {divisor} é {resultado}")
-    except ZeroDivisionError:
+        testa_divisao(0)
+    except AttributeError as E:
+        print('Erro de atributo tratado')
+    except ZeroDivisionError as E:
         print("Erro de divisão por 0 tratado")
-
-
-try:
-    testa_divisao(0)
-except AttributeError:
-    print('Erro de atributo tratado')
